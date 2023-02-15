27cddfe7a39e4d17253ed625aff6184a4b478ff9
XiaoYe <3285854830@qq.com>
2023-02-15 12:01:06 +0800

    完成分页查询

--- user/src/main/java/com/xiaoye/dao/BaseDao.java
+++ user/src/main/java/com/xiaoye/dao/BaseDao.java
@@ -4,21 +4,24 @@ package com.xiaoye.dao;
 import java.sql.*;
 
 public class BaseDao {
-    private static final String url = "jdbc:mysql://127.0.0.1:3306/jsp_x?characterEncoding=utf-8&serverTimezone=GMT&useSSL=false";
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/jsp_x?useUnicode=true&useSSL=false&characterEncoding=UTF8&serverTimezone=UTC";
     private static final String name = "root";
     private static final String password = "123456";
 
     public static Connection getcon() {
         Connection connection = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(url, name, password);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
         return connection;
     }
 
+    public static void main(String[] args) {
+        System.out.println(getcon());
+    }
     public static void closeAll(ResultSet rs, Statement sta, Connection connection) {
         try {
             if (rs != null) {
